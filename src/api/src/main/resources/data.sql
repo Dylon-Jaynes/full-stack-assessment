c52bbe342aec817770ba71dfd1be4039b2ac02d7
Dylon Jaynes <dylonjaynes@gmail.com>
2021-11-15 14:34:54 -0700

    Adding employees table with id, department_id, name, email, and phone and linking it to departments table.

--- src/api/src/main/resources/data.sql
+++ src/api/src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO departments(name) values ('engineering');
+INSERT INTO departments(name) values ('sales');
+INSERT INTO departments(name) values ('marketing');
--- src/api/src/main/resources/schema.sql
+++ src/api/src/main/resources/schema.sql
@@ -3,3 +3,13 @@ CREATE TABLE departments(
     id int AUTO_INCREMENT  PRIMARY KEY,
     name varchar(1000) NOT NULL
 );
+
+DROP TABLE IF EXISTS employees
+CREATE TABLE employees(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    department_id int NOT NULL,
+    name varchar(1000) NOT NULL,
+    email varchar(100),
+    phone varchar(15),
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+)
